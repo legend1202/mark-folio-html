ff7d695d2cf5a08ca3df802d1486ee9708c7a815
lagend <markdrake0916@gmail.com>
2023-08-23 11:11:43 -0500

    create:first version

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+AOS.init();
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 700, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+
+const contact = document.querySelector('.contact')
+const contactbtn = document.querySelector('.contact-btn')
+const currentYear = document.querySelector('.current-year')
+
+contact.addEventListener('click', function(){
+window.location.href="mailto:matt.ehrlich93@yahoo.com"
+})
+
+contactbtn.addEventListener('click', function(){
+window.location.href="mailto:matt.ehrlich93@yahoo.com"
+})
+
+const year = new Date().getFullYear()
+
+function returnYear(){
+  currentYear.innerHTML = year
+}
+returnYear()
